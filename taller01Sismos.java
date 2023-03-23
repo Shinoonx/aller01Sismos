e587b7720dbd56575d75214858468dc60e828c25
Shinoonx <128055374+Shinoonx@users.noreply.github.com>
2023-03-23 10:54:36 -0300

    Update taller01Sismos.java

--- taller01Sismos.java
+++ taller01Sismos.java
@@ -1,13 +1,14 @@
 public class Taller01Sismos {
-   public static void main (String[]arg){
+   public static void main (String[]arg) {
       double[][] matriz = new double[7][15];
-      imprimir(matriz);
       mat(matriz);
+      imprimir(matriz);
+      imprMax(matriz);
    }
    public static void mat(double matriz[][]){
       for(int i=0;i < matriz.length;i++){
        for (int j=0;j < matriz.length;j++){
-          matriz[i][j]=(int)(Math.random()*9.9);
+          matriz[i][j] = Math.random()*9.9;
        }
     }
    }
@@ -20,4 +21,33 @@ public class Taller01Sismos {
       }
       System.out.println("\n");
    }
+
+   public static double max(double matriiz [][]){
+           double max= 0.0;
+      for (int i = 0; i < matriiz.length ; i++) {
+         for (int j = 0; j < matriiz[i].length; j++) {
+            if (max<matriiz[i][j]){
+               max=matriiz[i][j];
+            }
+         }
+      }
+      return max;
+   }
+   public static void imprMax(double matriiz[][]){
+   double max = max(matriiz);
+      System.out.println("EL mayor sismo fue de; "+ max);
+   }
+
+   public static int cinco(double matriz){
+       int num=0;
+      for (int i = 0; i < matriz.length; i++) {
+         for (int j = 0; j < matriz[0].length; j++) {
+            if (cinco)
+
+         }
+
+      }
+   }
+
+
 }
